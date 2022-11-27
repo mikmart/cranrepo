facd4b728ed86bf9b2842cccd1de0a08ad651fd1
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2022-11-27 14:37:32 +0000

    Refine documentation website

--- R/repo.R
+++ R/repo.R
@@ -2,15 +2,18 @@
 #'
 #' Set up the directory structure and package indexes for
 #' a CRAN-like package repository suitable to [install.packages()] from.
-#' 
+#'
 #' Binary distribution trees are always created to avoid errors
 #' when the repository is accessed via the `file://` protocol.
-#' 
+#'
 #' @param dir Path to the directory to use as the repository root.
 #'   Will be created if it does not exist.
 #' @param r_version The version of R to create binary distribution trees for.
 #'
 #' @return Path to the repository root directory, invisibly.
+#' @seealso [repo_serve()] to serve packages from the repository over HTTP.
+#' @family functions to manage repositories
+#' @concept manage
 #' @export
 repo_create <- function(dir = ".", r_version = getRversion()) {
   repo <- fs::dir_create(dir)
@@ -33,11 +36,11 @@ repo_packages_path <- function(repo, type, r_version = getRversion()) {
 
 PACKAGE_TYPES <- c("source", "win.binary", "mac.binary")
 
-#' Insert a package into a repository
+#' Insert a package bundle into a repository
 #'
 #' You can insert multiple packages with one call, as long as they are of the
 #' same type and for the same version of R.
-#' 
+#'
 #' @param repo Path to the root directory of the package repository.
 #' @param file Path to the source or binary package bundle to be added.
 #' @param type Type of packages. One of: `"source"`, `"win.binary"`, `"mac.binary"`.
@@ -45,6 +48,8 @@ PACKAGE_TYPES <- c("source", "win.binary", "mac.binary")
 #' @param replace Logical. Should the package be replaced if already present?
 #'
 #' @return Path(s) to the inserted package file(s), invisibly.
+#' @family functions to manage repositories
+#' @concept manage
 #' @export
 repo_insert <- function(repo, file, type, r_version = getRversion(), replace = FALSE) {
   dir <- repo_packages_path(repo, type, r_version)
@@ -54,10 +59,10 @@ repo_insert <- function(repo, file, type, r_version = getRversion(), replace = F
 }
 
 #' Remove a package from a repository
-#' 
+#'
 #' By default, shows a list of files that _would be_ removed.
 #' Explicitly specify `commit = TRUE` to actually remove them.
-#' 
+#'
 #' @inheritParams repo_insert
 #' @param package The name of the package to remove.
 #' @param version The version of the package to remove.
@@ -66,6 +71,8 @@ repo_insert <- function(repo, file, type, r_version = getRversion(), replace = F
 #'   removed, without actually removing them.
 #'
 #' @return Path(s) to the removed package file(s), invisibly.
+#' @family functions to manage repositories
+#' @concept manage
 #' @export
 repo_remove <- function(repo, package, version, type, r_version = getRversion(), commit = FALSE) {
   dir <- repo_packages_path(repo, type, r_version)
@@ -89,6 +96,8 @@ repo_remove <- function(repo, package, version, type, r_version = getRversion(),
 #'
 #' @inheritParams repo_insert
 #'
+#' @family functions to manage repositories
+#' @concept manage
 #' @export
 repo_update <- function(repo, type, r_version = getRversion()) {
   type <- rlang::arg_match0(type, PACKAGE_TYPES)
@@ -101,11 +110,12 @@ repo_update <- function(repo, type, r_version = getRversion()) {
 #'
 #' Serve packages from a repository over HTTP locally. For production use,
 #' you would likely have a different file server, but when setting up your
-#' repository it can be useful to test the behaviour over a web protocol. 
+#' repository it can be useful to test the behaviour over a web protocol.
 #'
 #' @inheritParams repo_insert
 #' @param ... Additional arguments passed on to [servr::httd()].
 #'
+#' @concept develop
 #' @export
 repo_serve <- function(repo, ...) {
   if (requireNamespace("servr", quietly = TRUE)) {
--- README.md
+++ README.md
@@ -7,9 +7,9 @@ cranrepo builds on [cranlike](https://cran.r-project.org/package=cranlike)
 to give you the basic tools to set up your own CRAN-like R package repository.
 You can:
 
-- Set up a repository directory structure with `repo_create()`.
-- Add a source or binary package bundle to a repository with `repo_insert()`.
-- Remove packages from a repository with `repo_remove()`.
+- Set up a repository directory structure and index with `repo_create()`.
+- Add source and binary package bundles to a repository with `repo_insert()`.
+- Remove versions of packages from a repository with `repo_remove()`.
 - Serve repositories over HTTP for development purposes with `repo_serve()`.
 
 ## Installation
