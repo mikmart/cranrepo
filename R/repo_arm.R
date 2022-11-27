8b8b4ae9d46608861f34066125e5e82eceff20fd
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2022-11-27 23:06:43 +0000

    Simplify repo arm handling

--- R/repo_arm.R
+++ R/repo_arm.R
@@ -1,59 +1,49 @@
 repo_arm <- function(root, type, r_version) {
-  type <- rlang::arg_match0(type, PACKAGE_TYPES, error_call = rlang::caller_env())
-  if (type != "source") {
-    os <- switch(type, win.binary = "windows", mac.binary = "macosx")
-    dir <- fs::path("bin", os, "contrib", numeric_version(r_version)[, 1:2])
-  } else {
-    dir <- fs::path("src", "contrib")
-  }
+  type <- rlang::arg_match0(type, PACKAGE_TYPES, "type", rlang::caller_env())
+  minor_r_version <- numeric_version(r_version)[, 1:2]
+  dir <- switch(type,
+    source = fs::path("src", "contrib"),
+    win.binary = fs::path("bin", "windows", "contrib", minor_r_version),
+    mac.binary = fs::path("bin", "macosx", "contrib", minor_r_version)
+  )
   structure(fs::path(root, dir), type = type)
 }
 
 PACKAGE_TYPES <- c("source", "win.binary", "mac.binary")
 
-repo_arm_path <- function(arm, ...) {
-  fs::path(arm, ...)
+repo_arm_path <- function(dir, ...) {
+  fs::path(dir, ...)
 }
 
-repo_arm_type <- function(arm) {
-  attr(arm, "type")
-}
-
-repo_arm_create <- function(arm) {
-  dir <- repo_arm_path(arm)
+repo_arm_create <- function(dir) {
   fs::dir_create(dir)
   package_index_create(dir)
   fs::path(dir)
 }
 
-repo_arm_insert <- function(arm, file) {
-  dir <- repo_arm_path(arm)
+repo_arm_insert <- function(dir, file) {
   fs::dir_create(dir)
   fs::file_copy(file, dir, overwrite = TRUE) -> dst
   package_index_insert(dir, fs::path_file(dst))
   fs::path(dst)
 }
 
-repo_arm_contains <- function(arm, file) {
-  dir <- repo_arm_path(arm)
+repo_arm_contains <- function(dir, file) {
   fs::path_file(file) %in% package_index_list(dir)
 }
 
-repo_arm_remove <- function(arm, package, version) {
-  dir <- repo_arm_path(arm)
+repo_arm_remove <- function(dir, package, version) {
   files <- package_index_find(dir, package, version)
   package_index_remove(dir, files)
   fs::path(dir, files)
 }
 
-repo_arm_find <- function(arm, package, version) {
-  dir <- repo_arm_path(arm)
+repo_arm_find <- function(dir, package, version) {
   files <- package_index_find(dir, package, version)
   fs::path(dir, files)
 }
 
-repo_arm_update <- function(arm) {
-  dir <- repo_arm_path(arm)
-  package_index_update(dir, repo_arm_type(arm))
+repo_arm_update <- function(dir) {
+  package_index_update(dir, attr(dir, "type"))
   invisible(NULL)
 }
